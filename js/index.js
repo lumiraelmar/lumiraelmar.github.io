9c3122b35953753479adfa26a4e53e6de9baeaaa
Lucia <lumiraelmar@gmail.com>
2020-05-23 19:38:20 +0200

    added series preview

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 const header = document.querySelector('.header');
 const sticky = header.offsetTop;
 
+const series = document.querySelectorAll(".series-js");
+const link = document.querySelectorAll(".link");
+const hero = document.querySelector(".hero-img");
+const preview = document.querySelector(".series-preview");
+const main = document.querySelector(".main-js");
+
 function stickyNavigation() {
   if (window.scrollY > sticky) {
     header.classList.add('sticky');
@@ -9,3 +15,22 @@ function stickyNavigation() {
   }
 }
 window.addEventListener('scroll', stickyNavigation);
+
+function clickI() {
+
+  hero.classList.toggle("active");
+  preview.classList.toggle("active");
+}
+
+function home() {
+  hero.classList.toggle("active");
+  preview.classList.toggle("active");
+}
+
+series.forEach (function(item) {
+  item.addEventListener('click', clickI) 
+});
+
+link.forEach (function(item) {
+  item.addEventListener('click', home) 
+});
