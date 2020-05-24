059711cbd3fcf617fce3c0ed109ca1614de16d28
Lucia <lumiraelmar@gmail.com>
2020-05-24 18:53:29 +0200

    arregle todo lo que destrui

--- js/index.js
+++ js/index.js
@@ -1,20 +1,9 @@
-const header = document.querySelector('.header');
-const sticky = header.offsetTop;
-
 const series = document.querySelectorAll(".series-js");
 const link = document.querySelectorAll(".link");
 const hero = document.querySelector(".hero-img");
 const preview = document.querySelector(".series-preview");
-const main = document.querySelector(".main-js");
 
-function stickyNavigation() {
-  if (window.scrollY > sticky) {
-    header.classList.add('sticky');
-  } else {
-    header.classList.remove('sticky');
-  }
-}
-window.addEventListener('scroll', stickyNavigation);
+
 
 function clickI() {
 
