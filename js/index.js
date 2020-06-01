d975794455b4713ceae33552670baff6c7991585
Lucia <lumiraelmar@gmail.com>
2020-06-02 01:26:48 +0200

    hamburger menu closes when you click outside

--- js/index.js
+++ js/index.js
@@ -3,19 +3,35 @@ const navbar = document.querySelector(".navbar-list-js");
 const links = document.querySelectorAll(".navbar-list-item");
 
 
-menuIcon.onclick = function () {
+function toggleMenu() {
   if(navbar.classList.contains("open")) {
-    navbar.classList.remove("open")
-  } else {
-    navbar.classList.add("open")
-  }
-  }
-
-
-  links.forEach(link => link.addEventListener("click", ()=>{
     navbar.classList.remove("open");
-  }))
+  } else {
+    navbar.classList.add("open");
+  }
+}
 
+
+links.forEach(function (link) {
+  link.onclick = function () {
+    navbar.classList.remove("open");
+  }
+});
+
+//link.addEventListener("click", ()=>{
+
+  menuIcon.onclick = function (e) {
+    e.stopPropagation();
+    toggleMenu();
+  }
+
+  navbar.onclick = function (e) {
+    e.stopPropagation();
+  }
+
+  document.body.onclick = function () {
+    navbar.classList.remove("open");
+  }
 
 $(document).ready(function(){
   
